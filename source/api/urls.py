986e896f40f0d4552f6ae5fd62d155632a0ca997
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-16 14:34:08 +0600

    Добавил метод вычитания для первого задания

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add_view
+from .views import add_view, subtract_view
 
 app_name = 'api'
 
 urlpatterns = [
-    path('add/', add_view, name='add')
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract')
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -21,3 +21,22 @@ def add_view(request, *args, **kwargs):
             response.status_code = 400
             return response
 
+
+@csrf_exempt
+def subtract_view(request, *args, **kwargs):
+    if request.method == "POST":
+        if request.body:
+            try:
+                numbers = json.loads(request.body)
+                A = float(numbers.get("A"))
+                B = float(numbers.get("B"))
+                return JsonResponse({'answer': A - B})
+            except ValueError:
+                response = JsonResponse({'error': 'Passed not a number!'})
+                response.status_code = 400
+                return response
+        else:
+            response = JsonResponse({'error': 'Numbers not sent'})
+            response.status_code = 400
+            return response
+
