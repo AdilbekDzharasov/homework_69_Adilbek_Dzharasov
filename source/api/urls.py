2b3171a9dcbb5b8c4ade193ed1c886320dc7c305
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-16 16:41:30 +0600

    При добавлении метода деления для первого задания обнаружил дублирование кода. Изменил методы сложения, умножения и вычитания, добавил метод деления

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import add_view, subtract_view, multiply_view
+from .views import add_view, subtract_view, multiply_view, divide_view
 
 app_name = 'api'
 
 urlpatterns = [
     path('add/', add_view, name='add'),
     path('subtract/', subtract_view, name='subtract'),
-    path('multiply/', multiply_view, name='multiply')
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide'),
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,61 +1,58 @@
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+
+def main_view(request, method):
+    if request.method == 'POST':
+        if request.body:
+            try:
+                return calculator(method, request.body)
+            except ValueError:
+                response = JsonResponse({'error': 'Passed not a number!'})
+                response.status_code = 400
+                return response
+        else:
+            response = JsonResponse({'error': 'Numbers not sent'})
+            response.status_code = 400
+            return response
+
+
+def calculator(method, body):
+    numbers = json.loads(body)
+    A = float(numbers.get("A"))
+    B = float(numbers.get("B"))
+    if method == 'add':
+        return JsonResponse({'answer': (A + B)})
+    elif method == 'subtract':
+        return JsonResponse({'answer': A - B})
+    elif method == 'multiply':
+        return JsonResponse({'answer': "%.2f" % (A * B)})
+    elif method == 'divide':
+        if B == 0:
+            response = JsonResponse({'error': "Can't divide by zero"})
+            response.status_code = 400
+            return response
+        else:
+            return JsonResponse({'answer': "%.2f" % (A / B)})
 
 
 @csrf_exempt
 def add_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        if request.body:
-            try:
-                numbers = json.loads(request.body)
-                A = float(numbers.get('A'))
-                B = float(numbers.get('B'))
-                return JsonResponse({'answer': A + B})
-            except ValueError:
-                response = JsonResponse({'error': 'Passed not a number!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'Numbers not sent'})
-            response.status_code = 400
-            return response
+    return main_view(request, 'add')
 
 
 @csrf_exempt
 def subtract_view(request, *args, **kwargs):
-    if request.method == "POST":
-        if request.body:
-            try:
-                numbers = json.loads(request.body)
-                A = float(numbers.get("A"))
-                B = float(numbers.get("B"))
-                return JsonResponse({'answer': A - B})
-            except ValueError:
-                response = JsonResponse({'error': 'Passed not a number!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'Numbers not sent'})
-            response.status_code = 400
-            return response
+    return main_view(request, 'subtract')
 
 
 @csrf_exempt
 def multiply_view(request, *args, **kwargs):
-    if request.method == "POST":
-        if request.body:
-            try:
-                numbers = json.loads(request.body)
-                A = float(numbers.get("A"))
-                B = float(numbers.get("B"))
-                return JsonResponse({'answer': A * B})
-            except ValueError:
-                response = JsonResponse({'error': 'Passed not a number!'})
-                response.status_code = 400
-                return response
-        else:
-            response = JsonResponse({'error': 'Numbers not sent'})
-            response.status_code = 400
-            return response
+    return main_view(request, 'multiply')
+
+
+@csrf_exempt
+def divide_view(request, *args, **kwargs):
+    return main_view(request, 'divide')
 
